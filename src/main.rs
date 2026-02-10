82a879e9a2c557870f70d760c07beed9c9e6e7bd
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2026-02-10 06:46:53 +0000

    Add washerwoman rules.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+/// This is only supporting Trouble Brewing.
 #[allow(dead_code)]
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Player {
     Unresolved,
     Seat(i32),
 }
 
 #[allow(dead_code)]
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Townsfolk {
     Washerwoman,
     Librarian,
@@ -24,7 +25,7 @@ enum Townsfolk {
 }
 
 #[allow(dead_code)]
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Outsider {
     Butler,
     Saint,
@@ -33,7 +34,7 @@ enum Outsider {
 }
 
 #[allow(dead_code)]
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Minion {
     Baron,
     Poisoner,
@@ -42,13 +43,13 @@ enum Minion {
 }
 
 #[allow(dead_code)]
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Demon {
     Imp,
 }
 
 #[allow(dead_code)]
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Character {
     GoodTownsfolk(Townsfolk),
     GoodOutsider(Outsider),
@@ -63,13 +64,16 @@ enum LiveOrDie {
     Dies,
 }
 
+// This is the player visible log.
+// Some of this might come out publicily.
+
 #[allow(dead_code)]
 #[derive(Debug)]
 enum Log {
     WasherwomanSees(Player, Player, Townsfolk), // https://wiki.bloodontheclocktower.com/Washerwoman
     LibrarianSees(Player, Player, Outsider),    // https://wiki.bloodontheclocktower.com/Librarian
     InvestigatorSees(Player, Player, Minion), // https://wiki.bloodontheclocktower.com/Investigator
-    ChefSees(i32),                            // https://wiki.bloodontheclocktower.com/Chef
+    Chef(i32),                                // https://wiki.bloodontheclocktower.com/Chef
     Empath(Player, Player, i32),              // https://wiki.bloodontheclocktower.com/Empath
     FortuneTeller(Player, Player, bool), // https://wiki.bloodontheclocktower.com/Fortune_Teller
     UndertakerSees(Player, Character),
@@ -91,10 +95,67 @@ enum Log {
     ImpKills(Player),
 }
 
+#[allow(dead_code)]
+#[derive(Debug)]
+enum ReportLog {
+    OnTime(Player, Log),
+}
+
 fn main() {
     println!("Hello, world!");
     let alice = Player::Seat(1);
     let bob = Player::Seat(2);
     let log = Log::FortuneTeller(alice, bob, true);
     println!("{:?}", &log);
+}
+
+#[allow(dead_code)]
+enum Constraint {
+    Is(Player, Character),
+    IsPoisoned(Player),
+    IsDrunk(Player, Character),
+}
+
+#[allow(dead_code)]
+enum CompoundConstraint {
+    AllOf(Vec<Constraint>),
+    OneOf(Vec<Vec<Constraint>>),
+}
+
+
+fn expect(report_log : ReportLog) ->  CompoundConstraint {
+    use Character::GoodTownsfolk;
+    use Character::EvilMinion;
+    use Constraint::Is;
+    use Constraint::IsDrunk;
+    use Constraint::IsPoisoned;
+    use CompoundConstraint::OneOf;
+    use Townsfolk::Washerwoman;
+
+    return match report_log {
+        ReportLog::OnTime(washerwoman, Log::WasherwomanSees(a, b, townsfolk)) =>  OneOf(
+            vec![
+                vec![Is(washerwoman, GoodTownsfolk(Washerwoman)), Is(a, GoodTownsfolk(townsfolk))],
+                vec![Is(washerwoman, GoodTownsfolk(Washerwoman)), Is(b, GoodTownsfolk(townsfolk))],
+                vec![Is(washerwoman, GoodTownsfolk(Washerwoman)), Is(a, EvilMinion(Minion::Spy))],
+                vec![Is(washerwoman, GoodTownsfolk(Washerwoman)), Is(b, EvilMinion(Minion::Spy))],
+                vec![IsPoisoned(washerwoman)],
+                vec![IsDrunk(washerwoman, GoodTownsfolk(Washerwoman))],
+            ]),
+        _ => todo!(),
+    }
+}
+
+
+#[cfg(test)] // Only compiles when running 'cargo test'
+mod tests {
+    // use super::*; // Import names from the outer scope
+
+    #[test]
+    fn test_add() {
+        // Live and Imp-Person | NRB Play Blood On The Clocktower
+        // https://www.youtube.com/watch?v=m14N28Lq-jM
+        assert_eq!(1 + 1, 2);
+    }
+
 }
