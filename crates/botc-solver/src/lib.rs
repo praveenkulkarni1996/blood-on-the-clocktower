6c93418da93d6dee6c39f750f8915f9516c5dc76
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2026-05-03 16:33:38 +0100

    Move the setup code into a module.
    In a better future, we would move this into a crate of it's own. But
    baby steps for now.

--- crates/botc-solver/src/lib.rs
+++ crates/botc-solver/src/lib.rs
@@ -9,6 +9,8 @@ use botc_core::Player::Seat;
 use botc_core::{Character, Claim, Time, TimeIterator};
 use botc_core::{Player, ReportLog};
 
+pub mod setup;
+
 pub struct Registry<'ctx> {
     _context: &'ctx Context,
 
@@ -645,144 +647,6 @@ pub fn mark_characters_not_in_play(solver: &Solver, registry: &Registry, charact
     for c in characters.iter() {
         let _players = (0..registry.num_players).map(|seat| registry.get(Player::Seat(seat), *c));
         solver.assert(z3::ast::atmost(_players, 0));
-    }
-}
-
-pub mod setup {
-    use itertools::Itertools;
-
-    #[derive(Debug, Clone)]
-    struct PlayerCount {
-        townsfolk: i8,
-        outsider: i8,
-        minion: i8,
-        demon: i8,
-    }
-
-    #[rustfmt::skip]
-    static BASE_SETUP: &[PlayerCount] = &[
-        // 0-4 players is not a valid game.
-        PlayerCount { townsfolk: 0, outsider: 0, minion: 0, demon: 0}, // 0 invaild
-        PlayerCount { townsfolk: 0, outsider: 0, minion: 0, demon: 0}, // 1 invalid
-        PlayerCount { townsfolk: 0, outsider: 0, minion: 0, demon: 0}, // 2 invalid
-        PlayerCount { townsfolk: 0, outsider: 0, minion: 0, demon: 0}, // 3 invalid
-        PlayerCount { townsfolk: 0, outsider: 0, minion: 0, demon: 0}, // 4 invalid
-        // 5-player and 6-player are Teensyville.
-        // The minion and demon are not told each other.
-        PlayerCount { townsfolk: 3, outsider: 0, minion: 1, demon: 1}, // 5
-        PlayerCount { townsfolk: 3, outsider: 1, minion: 1, demon: 1}, // 6
-        // 1-minion evil team.
-        PlayerCount { townsfolk: 5, outsider: 0, minion: 1, demon: 1}, // 7
-        PlayerCount { townsfolk: 5, outsider: 1, minion: 1, demon: 1}, // 8
-        PlayerCount { townsfolk: 5, outsider: 2, minion: 1, demon: 1}, // 9
-        // 2 minion evil team.
-        PlayerCount { townsfolk: 7, outsider: 0, minion: 2, demon: 1}, // 10
-        PlayerCount { townsfolk: 7, outsider: 1, minion: 2, demon: 1}, // 11
-        PlayerCount { townsfolk: 7, outsider: 2, minion: 2, demon: 1}, // 12
-        // 3-minion evil team.
-        PlayerCount { townsfolk: 9, outsider: 0, minion: 3, demon: 1}, // 13
-        PlayerCount { townsfolk: 9, outsider: 1, minion: 3, demon: 1}, // 14
-        PlayerCount { townsfolk: 9, outsider: 2, minion: 3, demon: 1}, // 15
-    ];
-
-    fn is_baron(r: &super::Registry, p: botc_core::Player) -> z3::ast::Bool {
-        use botc_core::Character::*;
-        use botc_core::Evil::Minion;
-        use botc_core::Minion::*;
-
-        r.get(p, Evil(Minion(Baron))).clone()
-    }
-
-    fn is_minion(r: &super::Registry, p: botc_core::Player) -> z3::ast::Bool {
-        use botc_core::Character::*;
-        use botc_core::Evil::Minion;
-        use botc_core::Minion::*;
-
-        r.get(p, Evil(Minion(Baron)))
-            | r.get(p, Evil(Minion(Poisoner)))
-            | r.get(p, Evil(Minion(ScarletWoman)))
-            | r.get(p, Evil(Minion(Spy)))
-    }
-
-    fn is_demon(r: &super::Registry, p: botc_core::Player) -> z3::ast::Bool {
-        use botc_core::Character::*;
-        use botc_core::Demon::*;
-        use botc_core::Evil::Demon;
-
-        r.get(p, Evil(Demon(Imp))).clone()
-    }
-
-    fn is_townsfolk(r: &super::Registry, p: botc_core::Player) -> z3::ast::Bool {
-        use botc_core::Character::*;
-        use botc_core::Good::Townsfolk;
-        use botc_core::Townsfolk::*;
-
-        r.get(p, Good(Townsfolk(Washerwoman)))
-            | r.get(p, Good(Townsfolk(Librarian)))
-            | r.get(p, Good(Townsfolk(Investigator)))
-            | r.get(p, Good(Townsfolk(Chef)))
-            | r.get(p, Good(Townsfolk(Empath)))
-            | r.get(p, Good(Townsfolk(FortuneTeller)))
-            | r.get(p, Good(Townsfolk(Undertaker)))
-            | r.get(p, Good(Townsfolk(Monk)))
-            | r.get(p, Good(Townsfolk(Ravenkeeper)))
-            | r.get(p, Good(Townsfolk(Virgin)))
-            | r.get(p, Good(Townsfolk(Slayer)))
-            | r.get(p, Good(Townsfolk(Soldier)))
-            | r.get(p, Good(Townsfolk(Mayor)))
-    }
-
-    fn is_outsider(r: &super::Registry, p: botc_core::Player) -> z3::ast::Bool {
-        use botc_core::Character::*;
-        use botc_core::Good::Outsider;
-        use botc_core::Outsider::*;
-
-        r.get(p, Good(Outsider(Butler)))
-            | r.get(p, Good(Outsider(Drunk)))
-            | r.get(p, Good(Outsider(Recluse)))
-            | r.get(p, Good(Outsider(Saint)))
-    }
-
-    fn assert_player_count_by_predicate(
-        r: &super::Registry,
-        predicate: fn(&super::Registry, botc_core::Player) -> z3::ast::Bool,
-        count: i8,
-    ) -> z3::ast::Bool {
-        let zero = z3::ast::Int::from_i64(0);
-        let one = z3::ast::Int::from_i64(1);
-        let player_satisfies_predicate: Vec<z3::ast::Int> = (0..r.num_players)
-            .map(botc_core::Player::Seat)
-            .map(|p| predicate(r, p).ite(&one, &zero))
-            .collect_vec();
-
-        let want = z3::ast::Int::from_i64(count as i64);
-        z3::ast::Int::add(player_satisfies_predicate.iter().as_slice()).eq(want)
-    }
-
-    fn assert_setup(r: &super::Registry, setup: PlayerCount) -> z3::ast::Bool {
-        assert_player_count_by_predicate(r, is_townsfolk, setup.townsfolk)
-            & assert_player_count_by_predicate(r, is_demon, setup.demon)
-            & assert_player_count_by_predicate(r, is_minion, setup.minion)
-            & assert_player_count_by_predicate(r, is_outsider, setup.outsider)
-    }
-
-    pub fn assert_player_count_rules(r: &super::Registry) -> z3::ast::Bool {
-        let base_setup: PlayerCount = BASE_SETUP[r.num_players as usize].clone();
-        let baron_setup: PlayerCount = PlayerCount {
-            townsfolk: base_setup.townsfolk - 2,
-            outsider: base_setup.outsider + 2,
-            minion: base_setup.minion,
-            demon: base_setup.demon,
-        };
-
-        let has_base_setup = assert_setup(r, base_setup);
-        let has_baron_setup = assert_setup(r, baron_setup);
-        let baron_present = assert_player_count_by_predicate(r, is_baron, 1);
-        let baron_absent = assert_player_count_by_predicate(r, is_baron, 0);
-
-        (has_base_setup.clone() ^ has_baron_setup.clone())
-            & has_base_setup.implies(baron_absent)
-            & has_baron_setup.implies(baron_present)
     }
 }
 
