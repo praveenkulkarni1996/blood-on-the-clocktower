7c12a20fc7b052747ecd2839180a0dca6f3f8a28
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2026-05-03 03:13:38 +0100

    Partially correct the hallucinated LLM youtube log.

--- src/main.rs
+++ src/main.rs
@@ -20,60 +20,61 @@ fn setup_game() -> Vec<botc::ReportLog> {
     let dom = Player::Seat(8); // Mayor
     let blair = Player::Seat(9); // Imp
 
+    use Claim::*;
+    use Outsider::*;
+    use ReportLog::*;
+
     vec![
         // NIGHT-1
         // Sullivan (Poisoner) selects a player to neutralize.
-        // ReportLog::OnTime(Night(1), sullivan, Claim::PoisonerPoisons(isaac)),
+        // OnTime(Night(1), sullivan, Claim::PoisonerPoisons(isaac)),
         // Adam (Chef) learns how many pairs of evil players are sitting next to each other.
         // Could learn both 1 or 2.
-        ReportLog::OnTime(Night(1), adam, Claim::ChefGets(1)),
+        OnTime(Night(1), adam, ChefGets(1)),
+        //
         // 3. John (Librarian) learns that either Adam or Laurie is the Recluse.
-        ReportLog::OnTime(
-            Night(1),
-            john,
-            Claim::LibrarianSees(adam, laurie, Outsider::Recluse),
-        ),
-        // 4. Carly (Fortune Teller) selects herself and another player.
-        // She receives a "Yes" because she is her own Red Herring.
-        ReportLog::OnTime(Night(1), carly, Claim::FortuneTellerYes(adam, laurie)),
+        OnTime(Night(1), john, LibrarianSees(adam, laurie, Recluse)),
+        //
+        // 4. Carly (Fortune Teller) selects adam and RECLUSE laurie, and gets YES.
+        OnTime(Night(1), carly, Claim::FortuneTellerYes(adam, laurie)),
         // 5. Brooke (Drunk) believes she is the Undertaker.
         // 6. Blair (Imp) learns her Minions (Ollie, Sullivan) and Demon Bluffs (Investigator,
         //    Empath, Saint).
 
         // DAY-1
-        // Laurie (the Recluse) is executed.
-        ReportLog::DayExecutes(Time::Day(1), laurie),
+        // Adam (the Chef) is executed.
+        DayExecutes(Time::Day(1), adam),
         // NIGHT-2
         // 1. Sullivan (Poisoner) poisons Isaac (the Monk).
-        // ReportLog::OnTime(Night(2), sullivan, Claim::PoisonerPoisons(isaac)),
+        // OnTime(Night(2), sullivan, Claim::PoisonerPoisons(isaac)),
         // 2. Isaac (Monk) protects Carly (the Fortune Teller). (Invalidated by poison)
-        // ReportLog::OnTime(Night(2), isaac, Claim::MonkProtects(carly)),
-        // 3. Carly (Fortune Teller) selects herself and Dom. (Yes - Red Herring)
-        ReportLog::OnTime(Night(2), carly, Claim::FortuneTellerYes(carly, dom)),
-        // 4. Blair (Imp) kills John (the Librarian).
-        ReportLog::NightKilled(Night(2), john),
-        // 5. Brooke (Undertaker - Drunk) sees Laurie as the Recluse. (Truth)
-        ReportLog::OnTime(
+        // OnTime(Night(2), isaac, Claim::MonkProtects(carly)),
+        NightKilled(Night(2), isaac),
+        //
+        // Carly (Fortune Teller) selects herself (red-herring) and Sullivan.
+        OnTime(Night(2), carly, Claim::FortuneTellerYes(carly, sullivan)),
+        // 5. Brooke (Undertaker - Drunk) sees Adam as the chef.
+        OnTime(
             Night(2),
             brooke,
-            Claim::UndertakerSees(laurie, Character::Good(Good::Outsider(Outsider::Recluse))),
+            Claim::UndertakerSees(adam, Character::Good(Good::Townsfolk(Townsfolk::Chef))),
         ),
         // DAY-2
         // Adam (the Chef) is nominated and executed.
-        ReportLog::DayExecutes(Day(2), adam),
+        DayExecutes(Day(2), adam),
         // NIGHT-3
         // 1. Sullivan (Poisoner) poisons Carly (the Fortune Teller). (Disables Red Herring)
-        // ReportLog::OnTime(Day(2), sullivan, Claim::PoisonerPoisons(carly)),
+        // OnTime(Day(2), sullivan, Claim::PoisonerPoisons(carly)),
         // 2. Isaac (Monk) protects Adam. (Wait, Adam is dead - protecting a corpse/sinking kill?)
         // In the game, Isaac protected someone, but Blair killed Brooke.
-        ReportLog::NightKilled(Night(3), brooke),
+        NightKilled(Night(3), brooke),
         // 3. Carly (Fortune Teller) selects herself and Dom. (No - Poisoned)
-        ReportLog::OnTime(Night(3), carly, Claim::FortuneTellerNo(carly, dom)),
+        OnTime(Night(3), carly, Claim::FortuneTellerNo(carly, dom)),
         // 4. Blair (Imp) kills Brooke (the Drunk).
-        // ReportLog::OnTime(Night(3), blair, Claim::ImpKills(brooke)),
+        // OnTime(Night(3), blair, Claim::ImpKills(brooke)),
         // DAY-3
         // Isaac (the Monk) is nominated and executed.
-        ReportLog::DayExecutes(Day(3), isaac),
+        DayExecutes(Day(3), isaac),
         // NIGHT-4
         // The Final Five (Blair, Sullivan, Ollie, Carly, Dom) remain.
         // There were no deaths this night.
@@ -81,63 +82,63 @@ fn setup_game() -> Vec<botc::ReportLog> {
         // DAY-4
         // Carly (the Fortune Teller) is executed.
         // Result: Leaving only 4 alive (Blair, Sullivan, Ollie, Dom).
-        ReportLog::DayExecutes(Day(4), carly),
+        DayExecutes(Day(4), carly),
         // NIGHT-5
         // 1. Sullivan (Poisoner) poisons Dom (the Mayor).
         // Consequence: Disables the "bounce" safety net.
-        // ReportLog::OnTime(Night(5), sullivan, Claim::PoisonerPoisons(dom)),
+        // OnTime(Night(5), sullivan, Claim::PoisonerPoisons(dom)),
         // 2. Blair (Imp) kills Dom (the Mayor).
         // Result: Dom dies, leaving only the 3 Evil players.
-        ReportLog::NightKilled(Night(5), dom),
+        NightKilled(Night(5), dom),
         // Claims
-        ReportLog::OnTime(
+        OnTime(
             Night(1),
             ollie,
             Claim::Am(Character::Good(Good::Townsfolk(Townsfolk::Soldier))), /* Lying. Ollie is
                                                                               * a Baron. */
         ),
-        ReportLog::OnTime(
+        OnTime(
             Night(1),
             carly,
             Claim::Am(Character::Good(Good::Townsfolk(Townsfolk::FortuneTeller))),
         ),
-        ReportLog::OnTime(
+        OnTime(
             Night(1),
             john,
             Claim::Am(Character::Good(Good::Townsfolk(Townsfolk::Librarian))),
         ),
-        ReportLog::OnTime(
+        OnTime(
             Night(1),
             brooke,
             Claim::Am(Character::Good(Good::Townsfolk(Townsfolk::Undertaker))),
         ),
-        ReportLog::OnTime(
+        OnTime(
             Night(1),
             adam,
             Claim::Am(Character::Good(Good::Townsfolk(Townsfolk::Chef))),
         ),
-        ReportLog::OnTime(
+        OnTime(
             Night(1),
             isaac,
             Claim::Am(Character::Good(Good::Townsfolk(Townsfolk::Monk))),
         ),
-        ReportLog::OnTime(
+        OnTime(
             Night(1),
             sullivan,
             Claim::Am(Character::Good(Good::Townsfolk(Townsfolk::Empath))), /* Lying: Actually
                                                                              * poisoner */
         ),
-        ReportLog::OnTime(
+        OnTime(
             Night(1),
             laurie,
             Claim::Am(Character::Good(Good::Outsider(Outsider::Recluse))),
         ),
-        ReportLog::OnTime(
+        OnTime(
             Night(1),
             dom,
             Claim::Am(Character::Good(Good::Townsfolk(Townsfolk::Mayor))),
         ),
-        ReportLog::OnTime(
+        OnTime(
             Night(1),
             blair,
             Claim::Am(Character::Good(Good::Outsider(Outsider::Saint))), // Lying
