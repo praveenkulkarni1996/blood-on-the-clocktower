5c3c8ffd96d2534b7c09caae3850a00f712758be
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2026-05-04 01:37:19 +0100

    Add a puzzle from reddit.

---
+++ crates/botc-solver/examples/reddit.rs
@@ -0,0 +1,74 @@
+use botc_core::Time::*;
+use botc_core::*;
+
+/// Taken from Reddit, based on this puzzle by /u/ExcessiveUsernames.
+/// https://www.reddit.com/r/BloodOnTheClocktower/comments/1f6lgjv/trouble_brewing_puzzle/
+///
+/// This seems to allow for many different solutions.
+
+#[allow(unused_variables)]
+fn setup_game() -> Vec<botc_core::ReportLog> {
+    // Cyclic seating order starting from Erika
+    let erika = botc_core::Player::Seat(0);
+    let ailidh = botc_core::Player::Seat(1);
+    let john = botc_core::Player::Seat(2);
+    let rachael = botc_core::Player::Seat(3);
+    let you = botc_core::Player::Seat(4);
+    let edward = botc_core::Player::Seat(5);
+    let derek = botc_core::Player::Seat(6);
+    let kyle = botc_core::Player::Seat(7);
+    let linda = botc_core::Player::Seat(8);
+    let diane = botc_core::Player::Seat(9);
+
+    use Claim::*;
+    use Minion::*;
+    use Outsider::*;
+    use ReportLog::*;
+    use Townsfolk::*;
+
+    vec![
+        OnTime(Night(1), erika, Am(Character::Good(Good::Outsider(Saint)))),
+        OnTime(Night(1), ailidh, LibrarianSees(derek, rachael, Drunk)),
+        OnTime(Night(1), rachael, FortuneTellerYes(diane, linda)),
+        OnTime(Night(1), you, InvestigatorSees(edward, erika, Poisoner)),
+        OnTime(Night(1), edward, ChefGets(1)),
+        OnTime(Night(1), kyle, WasherwomanSees(ailidh, linda, Empath)),
+        OnTime(Night(1), linda, EmpathLearnsOne(diane, kyle)),
+        //
+        OnTime(Day(1), john, VirginKillsTownsfolk(ailidh)),
+        OnTime(Day(1), derek, SlayerMisses(diane)),
+        //
+        OnTime(
+            Night(2),
+            diane,
+            RavenkeeperSees(you, Character::Good(Good::Townsfolk(Investigator))),
+        ),
+    ]
+}
+
+fn main() {
+    let claim_logs = setup_game();
+
+    let solver = z3::Solver::new();
+    let registry = botc_solver::Registry::new(solver.get_context(), 10, Day(2));
+
+    let history = vec![];
+
+    for &log in claim_logs.iter() {
+        let ast_constraint = botc_solver::constrain(&registry, &history, &log);
+        solver.assert(ast_constraint);
+    }
+
+    botc_solver::player_has_exactly_one_character(&solver, &registry);
+    botc_solver::character_has_at_most_one_player(&solver, &registry);
+    botc_solver::atmost_one_player_can_be_red_herringed(&solver, &registry);
+    botc_solver::atmost_one_player_can_be_poisoned(&solver, &registry);
+    botc_solver::poisoner_can_poison_one_person_only_if_alive(&solver, &registry);
+    botc_solver::poisoning_does_not_move_during_the_day(&solver, &registry);
+    solver.assert(botc_solver::setup::assert_player_count_rules(&registry));
+    solver.assert(botc_solver::life::assert_life_until_death(&registry));
+
+    dbg!(solver.check());
+    let model = solver.get_model().expect("Failed to retrieve model");
+    dbg!(model);
+}
